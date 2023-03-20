d635fcf69694c2c1e586612c296755789368af3f
kategerasimenko <katgerasimenko@gmail.com>
2023-03-20 13:53:54 +0400

    change eval to use it from code

--- scripts/evaluator/evaluator.py
+++ scripts/evaluator/evaluator.py
@@ -9,14 +9,10 @@ import json
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
-def main():
-    parser = argparse.ArgumentParser(description='Evaluate leaderboard predictions for code completion (line level).')
-    parser.add_argument('--answers', '-a', required=True, help="filename of the labels, in json format.")
-    parser.add_argument('--predictions', '-p', required=True, help="filename of the leaderboard predictions, in txt format.")
-    args = parser.parse_args()
 
-    preds = open(args.predictions, "r").readlines()
-    gts = open(args.answers, "r").readlines()
+def evaluate(predictions, answers):
+    preds = open(predictions, "r").readlines()
+    gts = open(answers, "r").readlines()
 
     assert len(preds) == len(gts), f"Samples of predictions and answers are not equal, {len(preds)}: {len(gts)}"
 
@@ -26,17 +22,31 @@ def main():
         for pred, gt in zip(preds, gts):
             pred = pred.strip()
             gt = json.loads(gt)["code"]
-            wf.write(gt+"\n")
+            wf.write(gt + "\n")
             if pred.split() == gt.split():
                 EM += 1
 
-    bleu_score = round(_bleu("ground_truth.txt", args.predictions), 2)
-    logger.info(f"BLEU: {bleu_score}, EM: {round(EM/total*100, 2)}")
+    EM = round(EM / total * 100, 2)
+    bleu_score = round(_bleu("ground_truth.txt", predictions), 2)
+    logger.info(f"BLEU: {bleu_score}, EM: {EM}")
 
     try:
         os.remove("ground_truth.txt")
     except Exception:
         pass
+
+    return bleu_score, EM
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Evaluate leaderboard predictions for code completion (line level).')
+    parser.add_argument('--answers', '-a', required=True, help="filename of the labels, in json format.")
+    parser.add_argument('--predictions', '-p', required=True,
+                        help="filename of the leaderboard predictions, in txt format.")
+    args = parser.parse_args()
+
+    evaluate(args.predictions, args.answers)
+
 
 if __name__ == "__main__":
     main()
